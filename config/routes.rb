7b7466c78f20605fda228a2aa9fc1d22c0522323
Gilles Crofils <gilles@secondbureau.com>
2012-06-27 10:37:51 +0800

    #1107 - added :locale to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,23 @@
 Formicary::Engine.routes.draw do
 
   scope :module => 'Formicary' do
-    resources :nodes
-    resources :charts
 
-    match 'charts/:id' => 'charts#show', :as => :show_chart
-    match 'charts/:id' => 'charts#destroy', :as => :destroy_chart
-    match 'charts/:id/edit' => 'charts#edit', :as => :edit_chart
-    match 'charts/:id/build' => 'charts#build', :as => :build_chart
-    match 'charts/:id/deleteFromChart' => 'charts#deleteFromChart'
-    match 'charts/:id/nodeOnChart' => 'charts#nodeOnChart'
-    match 'charts/:id/loadNode' => 'charts#loadNode'
-    match 'charts/:id/loadNodes' => 'charts#loadNodes'
-    match 'nodes/:id' => 'nodes#destroy', :as => :destroy_node
+    scope "(/:locale)" do
+
+      resources :nodes
+      resources :charts
+
+      match 'charts/:id' => 'charts#show', :as => :show_chart
+      match 'charts/:id' => 'charts#destroy', :as => :destroy_chart
+      match 'charts/:id/edit' => 'charts#edit', :as => :edit_chart
+      match 'charts/:id/build' => 'charts#build', :as => :build_chart
+      match 'charts/:id/deleteFromChart' => 'charts#deleteFromChart'
+      match 'charts/:id/nodeOnChart' => 'charts#nodeOnChart'
+      match 'charts/:id/loadNode' => 'charts#loadNode'
+      match 'charts/:id/loadNodes' => 'charts#loadNodes'
+      match 'nodes/:id' => 'nodes#destroy', :as => :destroy_node
+
+    end
   end
 
 end
