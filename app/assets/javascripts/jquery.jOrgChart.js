e065249e745fac865e47cea091c0c2792565eb8a
Romain Binaux <romain@secondbureau.com>
2012-11-20 17:02:33 +0800

    Improvements: smaller org

--- app/assets/javascripts/jquery.jOrgChart.js
+++ app/assets/javascripts/jquery.jOrgChart.js
@@ -116,6 +116,11 @@
     }
 
     $nodeDiv.attr('style', $node['decoration']);
+
+    if (!$node['to_url']) {
+      $nodeDiv.css('border', '2px solid #AAA');
+    }
+
     $nodeDiv.addClass("node");
     $nodeDiv.attr('id', 'p'+$node['id']);
 
@@ -260,7 +265,7 @@
         appendTo.append($leftLine);
 
         // Line to the parent
-        var bas_du_parent = $parentCoord.top + parseInt($parent.css('height'))+4;
+        var bas_du_parent = $parentCoord.top + parseInt($parent.css('height'))-4;
         $lineToParent = $("<div>");
         $lineToParent.css('position', 'absolute');
         $lineToParent.css('border-left', '2px solid #AAA');
@@ -315,10 +320,10 @@
 
           // Line between the horizontal line and the parent
           var bas_du_parent = $parentCoord.top + parseInt($parent.css('height'));
-          var top_trait_horizontal_parent = coord.top - 30;
+          var top_trait_horizontal_parent = coord.top - 22;
           $nodeJonctionLine = $("<div>").addClass('chart-line');
           $nodeJonctionLine.css('position', 'absolute');
-          $nodeJonctionLine.css('top', $parentCoord.top + parseInt($parent.css('height')) + 4 - topOffset);
+          $nodeJonctionLine.css('top', $parentCoord.top + parseInt($parent.css('height')) - 4 - topOffset);
           $nodeJonctionLine.css('border-left', '2px solid #AAA');
           $nodeJonctionLine.css('left', parentMiddle - leftOffset);
           $nodeJonctionLine.css('width', 2);
